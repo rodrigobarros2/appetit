b37a5364cb3cebbb11c255e1e610e5b5a747874e
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-13 02:29:56 -0300

    Update index.tsx

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Logo, Wrapper } from './styles';
 
-const Login: React.FC = () => {
-	return (
-		<Container>
-			<h1>Login</h1>
-		</Container>
-	);
-};
+import Form from '../../components/Form';
+
+const Login: React.FC = () => (
+	<Container>
+		<Wrapper>
+			<Logo />
+			<h3>Seja bem-vindo!</h3>
+			<p>
+				Nós sabemos a importância de estar sempre de barriga cheia e o
+				quanto isso pode ajudar no seu dia.
+			</p>
+			<Form />
+			<caption>
+				Infoway Gestão em Saúde ©, 2019.
+			</caption>
+		</Wrapper>
+	</Container>
+);
 
 export default Login;
