1e7fe3435fd044f8b3259aa09fe88167f40874dc
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-13 02:31:40 -0300

    Adding css variables and typography

--- src/pages/OrderDetails/styles.ts
+++ src/pages/OrderDetails/styles.ts
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const WrapperOne = styled.div`
+	background: blue;
+`;
 
+export const WrapperTwo = styled.div`
+	background: red;
+`;
+
+export const WrapperTree = styled.div`
+	background: green;
 `;
