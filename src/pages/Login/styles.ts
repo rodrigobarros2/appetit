d22c19f34338e3973e6becd734c0a95529c56b3f
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-13 02:30:46 -0300

    Update styles.ts

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,5 +1,58 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+import { AppetitIcon } from '../../styles/Icons';
 
+export const Container = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	background-color: var(--gray);
+	height: 100%;
+	width: 100%;
+`;
+
+export const Logo = styled(AppetitIcon)`
+	width: 112px;
+	height: 80px;
+	margin-bottom: 40px;
+
+/* Primary */
+
+	/* 	> path {
+		fill: #f00;
+	}
+
+	&:hover > path {
+		fill: #ff0;
+	} */
+`;
+
+export const Wrapper = styled.div`
+	display:flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+
+
+	h3 {
+		margin-bottom: 16px;
+		text-align: center;
+		color: rgba(0, 0, 0, 0.87);
+
+	}
+
+	p {
+		margin-bottom: 115px;
+		width:432px;
+		text-align: center;
+		color: rgba(0, 0, 0, 0.54);
+	}
+
+	caption {
+		color: rgba(0, 0, 0, 0.56);
+		margin-top: 64px;
+		font-size: 16px;
+		line-height: 24px;
+	}
 `;
