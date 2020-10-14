b885d00023eb7f545adc2a6fecbc44ac8cc32f21
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-14 07:25:24 -0300

    Update styles.ts

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,16 +1,32 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+	display: grid;
+	grid-template-columns: 342px 1fr;
+	grid-template-rows: 1fr;
+
+	height: 100%;
+	width: 100%;
+
+	@media (max-width: 1000px) {
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: 1fr;
+	}
 `;
 
 export const WrapperOne = styled.div`
-	background: blue;
+	@media (max-width: 1000px) {
+			display:none;
+	}
 `;
 
 export const WrapperTwo = styled.div`
-	background: red;
+
 `;
 
-export const WrapperTree = styled.div`
-	background: green;
+export const Button = styled.button`
+	width: 100%;
+	height: 200px;
+	border: 1px solid red;
 `;
