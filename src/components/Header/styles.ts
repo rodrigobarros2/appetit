f386f1b725b634d6df4a3bd514b27208e9059584
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-14 07:25:29 -0300

    Update styles.ts

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,98 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+import { AppetitIcon, MenuIcon, UserIcon, Indicador } from '../../styles/Icons';
 
+export const Container = styled.div`
+	background: var(--orange);
+	box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.25);
+	height: 100%;
 `;
+
+export const Wrapper = styled.div`
+	display:flex;
+	height: 100%;
+	flex-direction: column;
+	justify-content: space-between;
+
+	div {
+		display:flex;
+		justify-content: center;
+	}
+
+	ul{
+		flex-grow: 1;
+		cursor: pointer;
+		list-style:none;
+		display:flex;
+		text-transform: uppercase;
+		flex-direction: column;
+		background:var(--orange);
+	}
+
+	li {
+		display:flex;
+		align-items: center;
+		padding: 10px;
+		margin: 2px 0;
+		color: rgba(255, 255, 255, 0.54);
+		padding-left: 40px;
+		font-weight: 600;
+		font-size: 14px;
+		line-height: 22px;
+
+		&:first-child{
+			background: white;
+			color: var(--orange);
+			line-height: 2.5;
+		}
+
+		&:nth-child(2){
+			color: var(--white);
+			padding-left: 60px;
+		}
+
+		&:nth-child(3){
+			padding-left: 79px;
+		}
+
+		&:nth-child(4){
+			padding-top: 20px;
+		}
+	}
+
+	caption{
+		font-size: 12px;
+		line-height: 18px;
+		margin-bottom: 40px;
+		color: var(--white);
+	}
+`;
+
+export const Logo = styled(AppetitIcon)`
+	width: 112px;
+	height: 80px;
+	margin: 56px 0 80px;
+
+	> path {
+		fill: var(--white);
+	}
+`;
+
+export const IconMenu = styled(MenuIcon)`
+	margin-right: 28px;
+`;
+
+export const IconUser = styled(UserIcon)`
+	margin-right: 28px;
+	> path {
+		fill: var(--orange);
+	}
+`;
+
+export const InconIndicador = styled(Indicador)`
+	margin-right: 16px;
+	> path {
+		fill: var(--orange);
+	}
+`;
+
