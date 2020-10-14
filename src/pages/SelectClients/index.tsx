1130d4a0a9af3d16c119125a279411485869594b
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-14 07:24:57 -0300

    Update index.tsx

--- src/pages/SelectClients/index.tsx
+++ src/pages/SelectClients/index.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { Container } from './styles';
 
 const SelectClients: React.FC = () => {
-	return (
-		<Container>
-			<h1>SelectClients</h1>
-		</Container>
-	);
+  return (
+    <Container>
+      <h1>SelectClients</h1>
+    </Container>
+  );
 };
 
 export default SelectClients;
