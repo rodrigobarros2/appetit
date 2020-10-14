96bf7245ee78b06b038556ca1a5682bb6acb78e8
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-14 07:25:38 -0300

    Update styles.ts

--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -26,13 +26,13 @@ export const Container = styled.div`
 		font-size: 16px;
 	}
 
-	a {
+	a:first-child {
 		position: relative;
 		display:block;
-		margin: 60px 0 37px 0;
+		margin: 60px 0 30px 0;
 		text-decoration: none;
 		font-weight: 600;
-		font-size: 14px;
+		font-size: 12px;
 		line-height: 22px;
 		text-transform: uppercase;
 		color: rgba(0, 0, 0, 0.88);
@@ -42,7 +42,7 @@ export const Container = styled.div`
 export const Button = styled.button`
 	width: 328px;
 	height: 48px;
-	background: var(--orange-disable);
+	background: var(--orange);
 	border-radius: 100px;
 	text-transform: uppercase;
 	color: #FFFFFF;
@@ -50,3 +50,58 @@ export const Button = styled.button`
     align-items: center;
     justify-content: center;
 `;
+
+/* OutlinedInput */
+
+export const OutlinedInput = styled.div`
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%)
+	}
+
+	.input-group {
+		position: relative;
+	}
+
+	.input-group .input-area {
+		outline: none;
+		border: 1px solid rgba(0, 0, 0, 0.56);
+		padding: 16px 13px;
+		font-size: 18px;
+		border-radius: 5px;
+	}
+
+	.input-group .input-area:valid + .label {
+		top: -8px;
+		padding: 0 3px;
+		font-size: 14px;
+		color: rgba(0, 0, 0, 0.56);
+	}
+
+	.input-group .input-area:focus {
+		border: 2px solid var(--orange);
+
+	}
+
+	.input-group .label {
+		color: rgba(0, 0, 0, 0.87);
+		font-size: 16px;
+		font-family: Roboto, sans-serif;
+		font-style: normal;
+		font-weight: normal;
+		position: absolute;
+		top: 20px;
+		left: 13px;
+		background: var(--gray);
+	}
+
+	.input-group .input-area:focus + .label {
+		top: -8px;
+		padding: 0 3px;
+		font-size: 14px;
+		color: var(--orange);
+		background: var(--gray);
+	}
+`;
+
