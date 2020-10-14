a3a5707c5a7a83108ac36c4f863f681d98ef2ee0
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-14 07:24:44 -0300

    Update index.tsx

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-
-import OutlinedInput from '../../components/OutlinedInput';
-import { Container, Button } from './styles';
-
+import { Container, Button, OutlinedInput } from './styles';
 
 const Form: React.FC = () => {
 	return (
 		<Container>
 			<form onSubmit={e => e.preventDefault()}>
-				<OutlinedInput />
+				<OutlinedInput>
+					<div className="input-group">
+						<input type="text" className="input-area" required id="inputField" />
+						<label htmlFor="inputField" className="label">Email</label>
+					</div>
+					<div className="input-group">
+						<input type="password" className="input-area" required id="inputField" />
+						<label htmlFor="inputField" className="label">Senha</label>
+					</div>
+				</OutlinedInput>
 			</form>
 			<section>
 				<Link to="/"> recuperar minha senha</Link>
-				<Button>
-					entrar
-				</Button>
+				<Link to="/home">
+					<Button className="btn">
+						entrar
+					</Button>
+				</Link>
 			</section>
 		</Container >
 	);
