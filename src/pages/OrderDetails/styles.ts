bcc5b4ad6b2004af8bc31e26e1a5cc1944c56d1c
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-14 07:24:53 -0300

    Update styles.ts

--- src/pages/OrderDetails/styles.ts
+++ src/pages/OrderDetails/styles.ts
@@ -1,13 +1,19 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+	display: grid;
+	grid-template-columns: 1fr 2fr;
+	grid-template-rows: 1fr;
+	min-height: 100vh;
+	max-height: 100vh;
+	min-height: 100%;
+	max-height: 100%;
+`;
+
 export const WrapperOne = styled.div`
 	background: blue;
 `;
 
 export const WrapperTwo = styled.div`
 	background: red;
-`;
-
-export const WrapperTree = styled.div`
-	background: green;
 `;
