a0ac960ed871a782fdca159e885936c9664ebcc6
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-14 07:24:46 -0300

    Update index.tsx

--- src/pages/SelectProduct/index.tsx
+++ src/pages/SelectProduct/index.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { Container } from './styles';
 
 const SelectProduct: React.FC = () => {
-	return (
-		<Container>
-			<h1>SelectProduct</h1>
-		</Container>
-	);
+  return (
+    <Container>
+      <h1>SelectProduct</h1>
+    </Container>
+  );
 };
 
 export default SelectProduct;
