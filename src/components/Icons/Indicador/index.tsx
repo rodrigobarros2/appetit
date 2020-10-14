0a6077f187f74cb153173ceb89173b08cd63c5b3
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-14 07:25:27 -0300

    Create index.tsx

---
+++ src/components/Icons/Indicador/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Indicador: React.FC = (props) => {
+	return (
+		<svg width="2" height="21" viewBox="0 0 2 21" fill="none" {...props} xmlns="http://www.w3.org/2000/svg">
+			<rect y="0.779907" width="2" height="20" fill="white" />
+		</svg>
+	);
+};
