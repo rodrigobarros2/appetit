bdf959222cbf3e41a402aa62fb9787ca0864d62c
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-14 07:25:01 -0300

    Update Icons.ts

--- src/styles/Icons.ts
+++ src/styles/Icons.ts
@@ -1,1 +1,4 @@
 export { AppetitIcon } from '../components/Icons/AppetitIcon';
+export { MenuIcon } from '../components/Icons/MenuIcon';
+export { UserIcon } from '../components/Icons/UserIcon';
+export { Indicador } from '../components/Icons/Indicador';
