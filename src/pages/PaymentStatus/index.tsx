8a7028b9461a0b20df3f15126cf5ef38f95954dc
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-14 07:24:48 -0300

    Update index.tsx

--- src/pages/PaymentStatus/index.tsx
+++ src/pages/PaymentStatus/index.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { Container } from './styles';
 
 const PaymentStatus: React.FC = () => {
-	return (
-		<Container>
-			<h1>PaymentStatus</h1>
-		</Container>
-	);
+  return (
+    <Container>
+      <h1>PaymentStatus</h1>
+    </Container>
+  );
 };
 
 export default PaymentStatus;
