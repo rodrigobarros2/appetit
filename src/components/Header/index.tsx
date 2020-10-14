9dad5922b7bad17642b1face13df1e8d527d62c9
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-14 07:25:32 -0300

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Container } from './styles';
+import { Container, Logo, Wrapper, IconMenu, IconUser, InconIndicador } from './styles';
 
 const Header: React.FC = () => {
 	return (
 		<Container>
-			<h1>Header</h1>
-		</Container>
+			<Wrapper>
+				<div>
+					<Logo />
+				</div>
+				<ul>
+					<Link to="/home"><li><IconMenu />pedidos</li></Link>
+					<li><InconIndicador />em abertos</li>
+					<li>encerrados</li>
+					<li><IconUser />clientes</li>
+				</ul>
+				<caption>Infoway Gestão em Saúde ©, 2019.</caption>
+			</Wrapper>
+		</Container >
 	);
 };
 
