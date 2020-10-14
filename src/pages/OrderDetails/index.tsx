f0c0876db37ef401e41cc5812536049a772ee3f9
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-14 07:24:55 -0300

    Update index.tsx

--- src/pages/OrderDetails/index.tsx
+++ src/pages/OrderDetails/index.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
 
-import { Container } from '../_layout';
-import { WrapperOne, WrapperTwo, WrapperTree } from './styles';
+import { Container, WrapperOne, WrapperTwo } from './styles';
 
 const OrderDetails: React.FC = () => {
 	return (
 		<Container>
 			<WrapperOne>
-				<h1>OrderDetails</h1>
+				<h1>Home</h1>
 			</WrapperOne>
 			<WrapperTwo>
-				<h1>OrderDetails</h1>
+				<h1>Home</h1>
 			</WrapperTwo>
-			<WrapperTree>
-				<h1>OrderDetails</h1>
-			</WrapperTree>
 		</Container>
 	);
 };
