e4c0282f4821bafd6f480428f968052f7941cbf4
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-16 00:43:44 -0300

    Update Icons.ts

--- src/styles/Icons.ts
+++ src/styles/Icons.ts
@@ -2,3 +2,7 @@ export { AppetitIcon } from '../components/Icons/AppetitIcon';
 export { MenuIcon } from '../components/Icons/MenuIcon';
 export { UserIcon } from '../components/Icons/UserIcon';
 export { Indicador } from '../components/Icons/Indicador';
+export { PlusIcon } from '../components/Icons/PlusIcon';
+export { SearchIcon } from '../components/Icons/SearchIcon';
+export { FilterIcon } from '../components/Icons/FilterIcon';
+export { ReturnIcon } from '../components/Icons/ReturnIcon';
