ab3c33b8e91dabee1b15c6b471f2e31abb0a37d5
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-16 00:44:00 -0300

    Update styles.ts

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
 
+import { PlusIcon, FilterIcon, SearchIcon } from '../../styles/Icons';
+
 export const Container = styled.div`
 	display: grid;
 	grid-template-columns: 342px 1fr;
 	grid-template-rows: 1fr;
-
-	height: 100%;
-	width: 100%;
 
 	@media (max-width: 1000px) {
 		display: grid;
@@ -22,11 +21,110 @@ export const WrapperOne = styled.div`
 `;
 
 export const WrapperTwo = styled.div`
+	margin: 0 40px;
+
+	h3 {
+		margin-top: 72px;
+		line-height: 36px;
+		color: rgba(0, 0, 0, 0.88);
+	}
+`;
+
+export const DateInformation = styled.div`
+	display:flex;
+	margin-top: 24px;
+
+	span {
+		color: #6E6E6E;
+		font-weight: 200;
+	}
 
 `;
 
-export const Button = styled.button`
+export const OrderInformation = styled.div`
+	display:flex;
+	justify-content: space-between;
+	background: #FFFFFF;
+	border-radius: 5px;
+	box-shadow: 0px 0.5px 10px rgba(0, 0, 0, 0.1);
+	align-items:center;
+	padding: 17px;
+	margin: 16px 0 20px;
+
+	div {
+		flex:1;
+	}
+
+	img {
+		border-radius: 50%;
+		margin-right:17px;
+	}
+`;
+
+
+export const Button = styled.div`
 	width: 100%;
-	height: 200px;
-	border: 1px solid red;
+	height: 56px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	text-transform: uppercase;
+	background: #FFFFFF;
+	box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+	border-radius: 5px;
+	font-weight: 600;
+	font-size: 14px;
+	line-height: 22px;
+	color: rgba(0, 0, 0, 0.56);
+	margin-top: 24px;
+
+	p{
+		flex:1;
+	}
+
+	svg {
+		margin: 20px;
+	}
+
 `;
+
+export const FilterOrder = styled.div`
+	display:flex;
+	align-items: center;
+	justify-content: space-between;
+	border-bottom:1px solid rgba(0, 0, 0, 0.34);
+	margin-top: 24px;
+
+	input {
+		flex:1;
+		font-size: 16px;
+		line-height: 24px;
+		color: rgba(0, 0, 0, 0.54);
+		border: none;
+	}
+
+	svg {
+			margin:10px 11px;
+		}
+`;
+
+export const IconPlus = styled(PlusIcon)`
+
+	path{
+		font-style: normal;
+font-weight: normal;
+font-size: 24px;
+line-height: 36px;
+/* identical to box height, or 150% */
+
+
+/* Black 0.88% */
+
+color: rgba(0, 0, 0, 0.88);
+	}
+
+
+
+`;
+export const IconSearch = styled(SearchIcon)``;
+export const IconFilter = styled(FilterIcon)``;
