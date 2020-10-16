a4238cd735a66bda9b9c6327218a4f9afa71d64d
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-16 20:02:20 -0300

    Update index.tsx

--- src/pages/OrderDetails/index.tsx
+++ src/pages/OrderDetails/index.tsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../../components/Header';
+import NewOrder from '../../components/NewOrder';
+import api from '../../services/api';
 
-import { Container } from './styles';
+import { Container, IconReturn, OrderInformation, WrapperOne, WrapperTwo } from './styles';
+
+interface Orderdetails {
+	id: number;
+	date: string;
+	name: string;
+	price: number;
+	imgIten: string;
+}
 
 const OrderDetails: React.FC = () => {
+	const [orderdetails, setOrderdetails] = useState<Orderdetails[]>([]);
+
+	useEffect(() => {
+		async function loadApi() {
+			const response = await api.get("orderdetails");
+			setOrderdetails(response.data);
+			console.log(response.data);
+		}
+		loadApi();
+	}, []);
+
+	const formatter = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' });
+
 	return (
 		<Container>
-			<h1>OrderDetails</h1>
+			<WrapperOne>
+				<Header />
+			</WrapperOne>
+
+			<WrapperTwo>
+
+				<Link to="/ordersplaced">
+					<IconReturn />
+					<NewOrder name="Pedidos de Luiz" />
+				</Link>
+
+				{orderdetails.map((orderdetail) => (
+					<div key={orderdetail.id}>
+						<OrderInformation>
+							<h6>{orderdetail.date}</h6>
+							<div>
+								<img src={orderdetail.imgIten} alt="User" />
+								<section>
+									<h6>{orderdetail.name}</h6>
+									<span> {formatter.format(orderdetail.price)} </span>
+								</section>
+							</div>
+						</OrderInformation>
+					</div>
+				))}
+
+			</WrapperTwo>
 		</Container>
 	);
 };
