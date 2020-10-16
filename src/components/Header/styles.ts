141b42b08403e67ddf3204f237b9d5a4a36a38cb
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-16 00:45:46 -0300

    Update styles.ts

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,7 +5,9 @@ import { AppetitIcon, MenuIcon, UserIcon, Indicador } from '../../styles/Icons';
 export const Container = styled.div`
 	background: var(--orange);
 	box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.25);
-	height: 100%;
+	height:100%;
+	position: fixed;
+    width: 342px;
 `;
 
 export const Wrapper = styled.div`
@@ -60,9 +62,12 @@ export const Wrapper = styled.div`
 		}
 	}
 
-	caption{
+	p{
+		width: 100%;
 		font-size: 12px;
 		line-height: 18px;
+		font-weight: 400;
+		text-align:center;
 		margin-bottom: 40px;
 		color: var(--white);
 	}
@@ -84,9 +89,6 @@ export const IconMenu = styled(MenuIcon)`
 
 export const IconUser = styled(UserIcon)`
 	margin-right: 28px;
-	> path {
-		fill: var(--orange);
-	}
 `;
 
 export const InconIndicador = styled(Indicador)`
