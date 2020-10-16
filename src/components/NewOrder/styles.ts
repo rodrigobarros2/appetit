1f372ddd354f71a33621dee3af6d815bbcea4dd4
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-16 20:02:22 -0300

    Update styles.ts

--- src/components/NewOrder/styles.ts
+++ src/components/NewOrder/styles.ts
@@ -2,10 +2,12 @@ import styled from 'styled-components';
 import { ReturnIcon } from '../../styles/Icons'
 
 export const Container = styled.div`
-
-
 `;
 
-export const IconReturn = styled(ReturnIcon)`
+interface Props {
+	size?: string;
+}
 
+export const IconReturn = styled(ReturnIcon) <Props>`
+	margin: ${props => props.size};
 `;
