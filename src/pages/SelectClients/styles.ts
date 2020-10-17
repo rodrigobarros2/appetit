671138228e18de54aa43bdc6fb0cab65fcc55143
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-17 11:16:38 -0300

    Update styles.ts

--- src/pages/SelectClients/styles.ts
+++ src/pages/SelectClients/styles.ts
@@ -1,5 +1,126 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
 
+export const Container = styled.div`
+	display: grid;
+	grid-template-columns: 342px 1fr 1fr;
+	height: 100%;
+`;
+
+export const ContentOne = styled.div`
+	padding: 40px 40px 0 40px;
+
+	h4 {
+		margin-top:24px;
+	}
+
+	span {
+		display:block;
+		margin-top: 16px;
+		font-size: 16px;
+		line-height: 24px;
+		color: rgba(0, 0, 0, 0.56);
+	}
+
+	div:nth-child(4) {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		margin-top: 23px;
+
+		b {
+			font-size: 16px;
+			line-height: 24px;
+			color: rgba(0, 0, 0, 0.54);
+		}
+
+		h6 {
+			flex:1;
+		}
+
+		img {
+			padding: 0 16px 0 0;
+		}
+	}
+`;
+
+export const ContentTwo = styled.div`
+	margin-top: 16px;
+
+	div{
+		width: 100%;
+		height: 8px;
+		background: rgba(0, 0, 0, 0.08);
+	}
+`;
+
+export const Progreessbar = styled.div`
+	margin-top: 8px;
+	border-radius: 5px;
+	display: flex;
+	width:100%;
+	height: 12px;
+	background: rgba(0, 0, 0, 0.08);
+
+	div:nth-child(1)  {
+		width: 66.6%;
+		background: #FF8822;
+		border-radius: 5px 5px;
+	}
+`;
+
+export const ContentTree = styled.div`
+	padding: 24px 40px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+
+	h4 {
+		color: rgba(0, 0, 0, 0.88);
+	}
+
+	span {
+		display: block;
+		font-size: 20px;
+		line-height: 30px;
+		color: rgba(0, 0, 0, 0.56);
+	}
+`;
+
+export const Clients = styled.div`
+	display:flex;
+	padding: 10px 0;
+	align-items: center;
+	border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+	cursor: pointer;
+
+	img {
+		margin-right: 16px;
+	}
+`;
+
+export const WrapperOne = styled.div`
+
+`;
+
+export const WrapperTree = styled.div`
+	padding: 40px 40px 0 40px;
+	height: 100%;
+
+	section {
+		margin-top: 8px;
+
+		a{
+			color: inherit;
+		}
+
+	}
+
+	> h6 {
+		margin-top: 24px;
+	}
+`;
+
+export const WrapperTwo = styled.div`
+	background: var(--white);
 `;
