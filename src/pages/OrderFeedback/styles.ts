7cc47aa52b7f8a28c6214798fc1e3155c8da8292
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-17 23:47:41 -0300

    Update Styles OrderFedback

--- src/pages/OrderFeedback/styles.ts
+++ src/pages/OrderFeedback/styles.ts
@@ -2,18 +2,19 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 	display: grid;
-	grid-template-columns: 1fr 2fr;
-	grid-template-rows: 1fr;
-	min-height: 100vh;
-	max-height: 100vh;
-	min-height: 100%;
-	max-height: 100%;
+	grid-template-columns: 342px 1fr;
+	height: 100%;
 `;
 
-export const WrapperOne = styled.div`
-	background: blue;
+export const GridOne = styled.div`
 `;
 
-export const WrapperTwo = styled.div`
-	background: red;
+export const GridTwo = styled.div`
+	margin-top: -100px;
+	text-align: center;
+	display:flex;
+	flex-direction: column;
+	align-items:center;
+	justify-content: center;
 `;
+
