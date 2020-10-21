8f9b5e9d9d78f30d7686b2cc45d8c5f07aedd3e9
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-21 04:48:46 -0300

    Create styles.ts

---
+++ src/components/OrderBox/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	> div   {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		margin-top: 23px;
+	}
+
+	span {
+		display:block;
+		margin-top: 16px;
+		font-size: 16px;
+		line-height: 24px;
+		color: rgba(0, 0, 0, 0.56);
+	}
+
+	b {
+		font-size: 16px;
+		line-height: 24px;
+		color: rgba(0, 0, 0, 0.54);
+	}
+
+	h6 {
+		flex:1;
+	}
+
+	img {
+		padding: 0 16px 0 0;
+	}
+`;
