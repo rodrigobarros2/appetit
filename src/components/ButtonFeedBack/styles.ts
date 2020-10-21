cd0ae834784f1b1c807d4e164c904e96300b18e7
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-21 04:48:03 -0300

    Reponsive style.js

--- src/components/ButtonFeedBack/styles.ts
+++ src/components/ButtonFeedBack/styles.ts
@@ -4,7 +4,6 @@ interface Props {
 	primary?: boolean;
 }
 
-
 export const Container = styled.div`
 	p {
 		font-style: normal;
@@ -16,13 +15,20 @@ export const Container = styled.div`
 `;
 
 export const Button = styled.button<Props>`
-		background: ${props => props.primary ? "none" : "#FF8822"};
-		color: ${props => props.primary ? "black" : "var(--white)"};
-		border: ${props => props.primary ? "2px solid rgba(0, 0, 0, 0.88)" : "none"};
-		margin: ${props => props.primary ? " 40px 16px 0 0" : "40px 0 0 16px"};
+	background: ${props => props.primary ? "none" : "#FF8822"};
+	color: ${props => props.primary ? "black" : "var(--white)"};
+	border: ${props => props.primary ? "2px solid rgba(0, 0, 0, 0.88)" : "none"};
+	margin: ${props => props.primary ? " 40px 16px 0 0" : "40px 0 0 16px"};
+	text-transform: uppercase;
+	width: 328px;
+	height: 48px;
+	border-radius: 100px;
 
-		text-transform: uppercase;
-		width: 328px;
-		height: 48px;
-		border-radius: 100px;
+	@media (max-width: 1100px) {
+		display:flex;
+		flex-direction: column;
+		align-items:center;
+		justify-content: center;
+		margin: 16px 0px;
+	}
 `;
