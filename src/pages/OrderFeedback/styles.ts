048e2209d25493c95d8de68f06b19448f3054e6f
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-10-21 04:50:06 -0300

    Update styles.ts

--- src/pages/OrderFeedback/styles.ts
+++ src/pages/OrderFeedback/styles.ts
@@ -4,26 +4,34 @@ export const Container = styled.div`
 	display: grid;
 	grid-template-columns: 342px 1fr;
 	height: 100%;
+
+	@media (max-width: 728px) {
+		display: grid;
+		grid-template-columns: 1fr;
+	}
 `;
 
-export const GridOne = styled.div``;
+export const GridOne = styled.div`
+	@media (max-width: 728px) {
+		display:none;
+	}
+`;
 
 export const GridTwo = styled.div`
-
-	p {
-		display:block;
-		margin-top: 30px;
-	}
-
-	button > {
-		margin:20px;
-	}
-
 	margin-top: -100px;
 	text-align: center;
 	display:flex;
 	flex-direction: column;
 	align-items:center;
 	justify-content: center;
+
+	p {
+		display:block;
+		margin: 30px 0 70px;
+	}
+
+	button > {
+		margin:20px;
+	}
 `;
 
