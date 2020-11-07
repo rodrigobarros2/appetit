c2e73bfcabc998a0a2d058554b9b54fa1beecee9
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-11-06 21:03:54 -0300

    Adding: dynamic value

--- src/components/OrderBox/index.tsx
+++ src/components/OrderBox/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import CuzcuzIcon from '../../assets/cuzcuz.svg';
-
+import { useCount } from '../../context/Count';
 import { Container } from './styles';
 
 const OrderBox: React.FC = () => {
+	const { count } = useCount();
+
 	return (
 		<Container>
 			<h4>Produtos</h4>
 			<div>
 				<img src={CuzcuzIcon} alt="" />
-				<h6>2x Cuscuz completo</h6>
+				<h6>{count}x Cuscuz completo</h6>
 				<b>R$ 3,25</b>
 			</div>
 			<span>Não adicionar manteiga</span>
